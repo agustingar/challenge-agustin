ab881a1b55d0ea698c26dcd3b5b16bea32bbb31b
Agustín García <agustin.tavoite@gmail.com>
2022-11-21 00:47:58 +0100

    finish

--- src/components/Galery/Galery.jsx
+++ src/components/Galery/Galery.jsx
@@ -5,7 +5,7 @@ export default function Galery() {
 
     return (
         <>
-            <div className='container-gifs' style={{backgroundColor:'black', height:'94vh', marginTop:'0%',padding:'2rem 0 0 0'}}>
+            <div className='container-gifs' style={{backgroundColor:'black', height:'100vh', marginTop:'0%',padding:'2rem 0 0 0'}}>
                 <div style={{ color: 'white' }}>
                     {localStorage.getItem('gif') ? <img src={localStorage.getItem('gif')} width='300' alt={localStorage.getItem('title')} /> : ''}
                     {localStorage.getItem('url') ? <img src={localStorage.getItem('url')} width='300' alt={localStorage.getItem('title')} /> : ''}
